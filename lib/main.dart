5512ff55afdaf208de5cf9d346c51ac3a5783d15
Mohamed Riyas <mail@riyas.co.in>
2020-06-07 03:09:36 +0800

    Select City and API Call based on selected city added

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,3 @@
-// Flutter code sample for Card
-
-// This sample shows creation of a [Card] widget that shows album information
-// and two actions.
-
 import 'dart:async';
 import 'dart:convert';
 
@@ -26,16 +21,32 @@ class MyApp extends StatefulWidget{
 
 /// This is the stateless widget that the main application instantiates.
 class MyAppState extends State<MyApp> {
-  Future<Album> futureAlbum;
-
+  Future<WeatherData> futureWeatherData;
+  List<City>  _city = City.getCity();
+  List<DropdownMenuItem<City>> _dropdownMenuItems;
+  City _selectedCity;
   @override
   void initState() {
+    _dropdownMenuItems = buildDropDownMenuItems(_city);
+    _selectedCity = _dropdownMenuItems[0].value;
     _getLocation().then((value){
       print('Async done');
     });
     super.initState();
   }
 
+  List<DropdownMenuItem<City>> buildDropDownMenuItems(List cities){
+    List<DropdownMenuItem<City>> items = List();
+    for(City city in cities){
+      items.add(
+          DropdownMenuItem(
+              value: city,
+              child: Text(city.name,style: TextStyle(fontWeight: FontWeight.bold,color: Colors.white,fontSize: 20 )),
+          ),
+      );
+    }
+    return items;
+  }
   final Location location = Location();
 
   LocationData _location;
@@ -49,7 +60,7 @@ class MyAppState extends State<MyApp> {
       final LocationData _locationResult = await location.getLocation();
       setState(() {
         _location = _locationResult;
-        futureAlbum = fetchAlbum(_location.latitude,_location.longitude);
+        futureWeatherData = fetchWeatherData(_location.latitude,_location.longitude);
       });
     } on PlatformException catch (err) {
       setState(() {
@@ -58,24 +69,65 @@ class MyAppState extends State<MyApp> {
     }
   }
 
-  Future<Album> fetchAlbum(lat,lng) async {
+  Future<WeatherData> fetchWeatherData(lat,lng) async {
     final response = await http.get('https://api.openweathermap.org/data/2.5/weather?lat=$lat&lon=$lng&appid=4e483e8711bcf08b3f0e7070da639d80&units=metric');
 
     if (response.statusCode == 200) {
       // If the server did return a 200 OK response,
       // then parse the JSON.
-      return Album.fromJson(json.decode(response.body));
+      return WeatherData.fromJson(json.decode(response.body));
     } else {
       // If the server did not return a 200 OK response,
       // then throw an exception.
-      throw Exception('Failed to load album');
+      throw Exception('Failed to load weather data');
     }
   }
   static const String _title = 'Weather App';
+  onChangeCity(City selectedCity){
+    setState(() {
+      _selectedCity = selectedCity;
+    });
+
+    switch(selectedCity.name) {
+      case 'Kuala Lumpur': {
+        futureWeatherData = fetchWeatherData(3.1390,101.6869);
+      }
+      break;
+
+      case 'George Town': {
+        futureWeatherData = fetchWeatherData(5.411229,100.335426);
+      }
+      break;
+
+      case 'Johor Bahru': {
+        futureWeatherData = fetchWeatherData(1.4655,103.7578);
+      }
+      break;
+
+      default: {
+        //statements;
+      }
+      break;
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: _title,
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.blue,
+        canvasColor: Colors.blue,
+      ),
       home: Scaffold(
         //appBar: AppBar(title: const Text(_title)),
         body: Container(
@@ -93,20 +145,20 @@ class MyAppState extends State<MyApp> {
                 padding: EdgeInsets.all(45.0),
                 //margin: EdgeInsets.all(20.0),
 
-                height:200,
+                height:250,
                 width: 600,
                 //alignment: Alignment.center
                 child: new Column(
                   children: <Widget>[
+
                     new Text('Your Location',textDirection: TextDirection.ltr,textAlign: TextAlign.center,style: TextStyle(fontWeight: FontWeight.bold,color: Colors.white,fontSize: 50 )),
-                    new DropdownButton<String>(
-                      items: <String>['Kuala Lumpur', 'George Town', 'Johor Bahru'].map((String value) {
-                        return new DropdownMenuItem<String>(
-                          value: value,
-                          child: new Text(value),
-                        );
-                      }).toList(),
-                      onChanged: (_) {},
+                    SizedBox(height: 20.0,),
+                    DropdownButton(
+                      iconEnabledColor: Colors.blue,
+                      focusColor: Colors.blue[900],
+                      value: _selectedCity,
+                      items: _dropdownMenuItems,
+                      onChanged: onChangeCity,
                     ),
                   ],
                 ),
@@ -116,7 +168,7 @@ class MyAppState extends State<MyApp> {
                 child: new Column(
                   children: <Widget>[
                     new Card(
-                      margin: EdgeInsets.all(40.0),
+                      margin: EdgeInsets.all(20.0),
                       color: Colors.blue[900],
                       child: Column(
                         children: <Widget>[
@@ -139,8 +191,8 @@ class MyAppState extends State<MyApp> {
                             padding: EdgeInsets.all(10.0),
                             width: 350,
                             //height: 300,
-                            child: FutureBuilder<Album>(
-                              future: futureAlbum,
+                            child: FutureBuilder<WeatherData>(
+                              future: futureWeatherData,
                               builder: (context, snapshot) {
                                 if (snapshot.hasData) {
                                   return Text(snapshot.data.title,textDirection: TextDirection.ltr,textAlign: TextAlign.center,style: TextStyle(fontWeight: FontWeight.bold,color: Colors.white,fontSize: 30 ));
@@ -158,8 +210,8 @@ class MyAppState extends State<MyApp> {
                             padding: EdgeInsets.all(20.0),
                             width: 350,
                             height: 150,
-                            child: FutureBuilder<Album>(
-                              future: futureAlbum,
+                            child: FutureBuilder<WeatherData>(
+                              future: futureWeatherData,
                               builder: (context, snapshot) {
                                 if (snapshot.hasData) {
                                   return Text(snapshot.data.temp+'\u00B0C',textDirection: TextDirection.ltr,textAlign: TextAlign.center,style: TextStyle(fontWeight: FontWeight.bold,color: Colors.white,fontSize: 80 ));
@@ -188,19 +240,34 @@ class MyAppState extends State<MyApp> {
     );
   }
 }
-class Album {
+class WeatherData {
   final String temp;
   final int id;
   final String title;
 
-  Album({this.temp, this.id, this.title});
+  WeatherData({this.temp, this.id, this.title});
 
-  factory Album.fromJson(Map<String, dynamic> json) {
-    return Album(
+  factory WeatherData.fromJson(Map<String, dynamic> json) {
+    return WeatherData(
       temp: json['main']['temp'].toString(),
       id: json['id'],
       title: json['name'],
     );
+  }
+}
+
+class City{
+  int id;
+  String name;
+
+  City(this.id,this.name);
+
+  static List<City> getCity(){
+    return <City>[
+      City(1,'Kuala Lumpur'),
+      City(2,'George Town'),
+      City(3,'Johor Bahru'),
+    ];
   }
 }
 
